cadcbf862547c744b3de0799739d01fe818859b1
Serge Basangovs <sergeb@thedigitalbench.co.uk>
2023-03-16 16:37:06 +0000

    feat: api added seearch pokemons endpoint

---
+++ src/pages/api/search/index.ts
@@ -0,0 +1,22 @@
+import { NextApiHandler } from "next";
+import pokemon from "@/data/pokemon.json";
+
+interface IReturnType {}
+
+const handleSearch: NextApiHandler<IReturnType> = async (req, res) => {
+  if (req.method !== "GET") {
+    res.status(405).json({ error: "method not allowed" });
+    return;
+  }
+
+  const searchParams = req.query;
+  const name = searchParams.name as string | undefined;
+
+  const pokemonData = pokemon.filter((current) =>
+    current.name.toLowerCase().includes(name?.toLowerCase() || "")
+  );
+
+  res.status(200).json(pokemonData);
+};
+
+export default handleSearch;
