e8ff18138770bf1ff8c1abb4432a9f386f24c4d6
Serge Basangovs <serge.basangovs@bbc.com>
2023-08-29 14:12:35 +0100

    feat: pexels api env vars set, types def set

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,11 @@
+import type { Metadata } from "next";
+
 import './globals.css';
+
+export const metadata: Metadata = {
+  title: "Next.js Image Gallery",
+  description: "Image gallery using pexel api and Next.js"
+}
 
 type Props = {
   children: React.ReactNode;
@@ -12,7 +19,11 @@ export default function RootLayout({ children }: Props) {
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <main className="max-w-6xl mx-auto">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
---
+++ src/types/index.ts
@@ -0,0 +1,36 @@
+
+export interface ISource {
+    original: string;
+    large2x: string;
+    large: string;
+    medium: string;
+    small: string;
+    portrait: string;
+    landscape: string;
+    tiny: string;
+}
+
+export interface IImage {
+    id: number;
+    width: number;
+    height: number;
+    url: string;
+    photographer: string;
+    photographer_url: string;
+    photographer_id: number;
+    avg_color: string;
+    src: ISource;
+    liked: boolean;
+    alt: string;
+}
+
+export interface IListResponse {
+    page: number;
+    per_page: number;
+    photos: IImage[];
+    next_page: string;
+}
+
+export interface ISearchResponse extends IListResponse {
+    total_results: number;
+}
