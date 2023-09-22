5bf0cd964750dd3267a45a8e25be4feb13828d90
Serge Basangovs <serge.basangovs@bbc.com>
2023-09-22 16:31:46 +0100

    feat: initial project files copied

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import Link from "next/link";
 
-import { IPageProps } from "@/types";
+import { getProducts } from "@/lib";
+import ProductCard from "@/components/ProductCard";
 
-const Homepage = async ({ params, searchParams }: IPageProps) => {
+export default async function Home() {
+  const products = await getProducts();
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-center">
-        NEXT V13 POLYGON
-      </h1>
-    </>
+    <div className="flex flex-wrap gap-2">
+      <ul role="list" className="flex flex-row flex-wrap m-2">
+        {products.map((product) => (
+          <li key={product.id} className="md:w-1/3">
+            <Link href={`/products/${product.id}`}>
+              <ProductCard {...product} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-};
-
-export default Homepage;
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -5,3 +5,26 @@ export interface IPageProps<
   params: P;
   searchParams: Q;
 }
+
+export interface Review {
+  rating: number;
+  text: string;
+}
+
+export interface Product {
+  id: number;
+  image: string;
+  name: string;
+  price: number;
+  description: string;
+  reviews: Review[];
+}
+
+export interface Cart {
+  products: {
+    id: number;
+    name: string;
+    image: string;
+    price: number;
+  }[];
+}
