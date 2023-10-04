0af2312b3eba7ad357acd58eb080b993559f213b
Serge Basangovs <serge.basangovs@bbc.com>
2023-10-04 16:35:36 +0100

    feat: image grid and page layout styled

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Roboto_Condensed } from "next/font/google";
 import type { Metadata } from "next";
+import classnames from "classnames";
 
 import "./globals.css";
 import ImagesGrid from "@/components/ImagesGrid";
@@ -27,12 +28,21 @@ export default function RootLayout({ children }: Props) {
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body className={robotoFont.className}>
-        <header></header>
-        <main className="max-w-screen-xl mx-auto">
-          <ImagesGrid />
-          {children}
-        </main>
+      <body
+        className={classnames(
+          robotoFont.className,
+          "flex justify-center items-center min-h-screen"
+        )}
+      >
+        <div className="w-[1000px] flex flex-col items-center py-4">
+          <header className="w-full bg-slate-100 flex flex-col items-center py-10">
+            <h1 className=" text-2xl mb-5 underline decoration-3 decoration-blue-400">
+              DAVID CANDO MUSIC
+            </h1>
+            <ImagesGrid />
+          </header>
+          <main>{children}</main>
+        </div>
       </body>
     </html>
   );
--- src/components/ImagesGrid/index.tsx
+++ src/components/ImagesGrid/index.tsx
@@ -8,12 +8,18 @@ interface IProps {
 
 const ImagesGrid = ({}: IProps): JSX.Element => {
   return (
-    <div>
+    <div className="grid grid-cols-4 gap-1">
       {GRID_DATA_ITEMS.map(({ id, attributes }) => {
         return (
-          <div key={id}>
-            <Link href={attributes.href}>{attributes.text}</Link>
-          </div>
+          <Link
+            href={attributes.href}
+            key={id}
+            className={`${attributes.background} w-52 h-52 flex flex-col justify-end items-center border-[2px] border-black`}
+          >
+            <span className="w-full bg-slate-200/75 text-center mb-2 hover:text-white hover:bg-slate-700/75">
+              {attributes.text}
+            </span>
+          </Link>
         );
       })}
     </div>
