3cb3ada272e150d56361a6ea472d126e0d398ce0
Serge Basangovs <serge.basangovs@bbc.com>
2023-10-04 08:38:12 +0100

    feat: added static files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,19 @@
+import { Roboto_Condensed } from "next/font/google";
+import type { Metadata } from "next";
+
 import "./globals.css";
 import Navigation from "@/components/Navigation";
+
+export const metadata: Metadata = {
+  title: "Udemy course app",
+  description:
+    "Build a production Next.js and React 18 site, w/ server and client components, React 18 hooks and Next.js hooks, & more.",
+};
+
+const robotoFont = Roboto_Condensed({
+  weight: ["300", "400", "700"],
+  subsets: ["latin"],
+});
 
 type Props = {
   children: React.ReactNode;
@@ -13,7 +27,7 @@ export default function RootLayout({ children }: Props) {
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>
+      <body className={robotoFont.className}>
         <header>
           <Navigation />
         </header>
