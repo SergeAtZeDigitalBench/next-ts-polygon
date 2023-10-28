89680a5c30cf57a5bc97782d90f85e4411ba7e5f
Serge Basangovs <sergejs.basangovs@gmail.com>
2023-10-28 12:13:50 +0300

    feat: initial setup, basic form

---
+++ src/app/simple-form/page.tsx
@@ -0,0 +1,14 @@
+import SimpleForm from '@/components/SimpleForm'
+
+const Page = async () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center mb-4">
+        Simple form input demo
+      </h1>
+      <SimpleForm />
+    </>
+  )
+}
+
+export default Page
