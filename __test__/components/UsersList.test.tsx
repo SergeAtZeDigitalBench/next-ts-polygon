b1bed79c9a96a5b3a93059ebd396e9b643bbc379
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2023-11-07 08:17:52 +0000

    refactor: users list unit test

--- __test__/components/UsersList.test.tsx
+++ __test__/components/UsersList.test.tsx
@@ -1,6 +1,13 @@
 import { render, screen, within } from '@testing-library/react'
 
 import UsersList from '@/components/UsersList'
+import { IUser } from '@/types'
+
+const renderComponent = (users: IUser[] = []) => {
+  const { container } = render(<UsersList users={users} />)
+
+  return { container, users }
+}
 
 describe('UsersList', () => {
   const mockUsers = [
@@ -15,14 +22,14 @@ describe('UsersList', () => {
   ]
 
   it('should render the component', () => {
-    render(<UsersList users={[]} />)
+    renderComponent()
     const component = screen.getByTestId('UsersList')
 
     expect(component).toBeInTheDocument()
   })
 
   it('should render `No  users` message if the users list is empty', () => {
-    render(<UsersList users={[]} />)
+    renderComponent()
     const component = screen.getByTestId('UsersList')
 
     expect(component).toHaveTextContent(/no users/i)
@@ -32,7 +39,7 @@ describe('UsersList', () => {
   // however, if we select the all table rows it will account the header rows and maybe some other rows
   // but we would like to count the rows within the table body only, for this example
   it('should render users table if the users list is not empty', () => {
-    render(<UsersList users={mockUsers} />)
+    const { users } = renderComponent(mockUsers)
     const component = screen.getByRole('table')
     const bodyRows = within(screen.getByTestId('UsersList_tbody')).getAllByRole(
       'row'
@@ -42,14 +49,30 @@ describe('UsersList', () => {
     // 2. screen.logTestingPlaygroundURL() // 👍 logs all rendered html tree for this current test
 
     expect(component).toBeInTheDocument()
-    expect(bodyRows).toHaveLength(mockUsers.length)
+    expect(bodyRows).toHaveLength(users.length)
   })
 
   // as an alternative for the test case above, we can use `container.querySelector()`
   it('should render users table if the users list is not empty [:alternative] ', () => {
-    const { container } = render(<UsersList users={mockUsers} />)
+    const { container, users } = renderComponent(mockUsers)
     const bodyRows = container.querySelectorAll('tbody tr')
 
-    expect(bodyRows).toHaveLength(mockUsers.length)
+    expect(bodyRows).toHaveLength(users.length)
+  })
+
+  it('should render the name and email of each user', () => {
+    const { users } = renderComponent(mockUsers)
+
+    users.forEach(({ name, email }) => {
+      const componentName = screen.getByRole('cell', {
+        name,
+      })
+      const componentEmail = screen.getByRole('cell', {
+        name: email,
+      })
+
+      expect(componentName).toBeInTheDocument()
+      expect(componentEmail).toBeInTheDocument()
+    })
   })
 })
