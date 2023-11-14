01d064885e316de72b4520f2fb8ef0a777454746
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2023-11-14 14:48:10 +0000

    feat: auth route, callbacks

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import { JWT } from "next-auth/jwt";
 
 import { verifyUserCredentials } from "@/lib/api";
 
@@ -19,8 +20,38 @@ export const nextAuthOptions: NextAuthOptions = {
       },
     }),
   ],
+  callbacks: {
+    /**
+     * The jwt() callback is called with user when the user first logs in.
+     * The `user` object will be populated with the object that is returned from the authorize function.
+     * The object that is returned from the jwt() callback is what will be saved on the session cookie.
+     */
+    async jwt({ token, user, account, profile }) {
+      if (typeof user !== "undefined") {
+        // user has just signed in so the user object is populated
+        return user as unknown as JWT;
+      }
+      return token;
+    },
+
+    /**
+     * The session() callback receives the session cookie content in its `token` parameter.
+     * Whatever is returned from this callback -
+     * is what will be presented when `useSession` or `getServerSession` is called.
+     */
+    async session({ session, token, user, newSession, trigger }) {
+      const { email, name, sub } = token;
+
+      const userSession = { ...session, email, name, sub };
+      return userSession;
+    },
+  },
 };
 
 const handler = NextAuth(nextAuthOptions);
 
 export { handler as GET, handler as POST };
+
+/**
+ * article: https://medium.com/ascentic-technology/authentication-with-next-js-13-and-next-auth-9c69d55d6bfd
+ */
