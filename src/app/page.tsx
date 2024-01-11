41627f3ce5b0ce4936c5a53c649efd553b5da815
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2024-01-11 16:19:03 +0000

    chore: next.js v14 installed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-
 import { IPageProps } from '@/types'
 
 const Homepage = async ({ params, searchParams }: IPageProps) => {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-center">
-        NEXT V13 POLYGON
+        NEXT V.14 POLYGON
       </h1>
     </>
   )
