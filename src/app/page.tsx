a7996c65030cbda753d7e72e8060723091a3ad5a
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2024-04-10 14:52:11 +0100

    fix: pets fetch 2 components client and server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,14 @@
 import PetsList from '@/components/PetsList'
-import { getPets } from '@/lib/fetch'
+import PetsListSsr from '@/components/PetsList/PetsListSsr'
 
 const Homepage = async () => {
-  const [pets, error] = await getPets()
-
-  console.log('pets :>> ', { pets, error })
-
-  console.log('process.env.NEXT_RUNTIME :>> ', process.env.NEXT_RUNTIME)
-
   return (
     <div className="max-w-5xl mx-auto">
       <h1 className="text-3xl font-bold underline text-center">
         NEXT V.14 POLYGON
       </h1>
-      <h2 className="text-xl font-bold text-center underline">Server fetch:</h2>
-      <ul className="border-b-2 border-gray-500 p-2 pl-4 my-4 list-disc">
-        {pets &&
-          pets.map((pet) => {
-            return <li key={pet}>{pet}</li>
-          })}
-
-        {error && (
-          <li className="text-center text-sm my-2 font-semibold text-red-700">
-            {error}
-          </li>
-        )}
-      </ul>
-      <h2 className="text-xl font-bold text-center underline my-4">
-        Browser fetch:
-      </h2>
+      {/* @ts-ignore server component */}
+      <PetsListSsr />
       <PetsList />
     </div>
   )
--- src/components/PetsList/index.tsx
+++ src/components/PetsList/index.tsx
@@ -40,23 +40,27 @@ const PetsList = (): JSX.Element => {
     }
   }, [])
   return (
-    <>
+    <div className="p-2 bg-slate-200 rounded-lg my-4 max-w-5xl mx-auto">
+      <h2 className="my-4 text-center text-xl font-semibold">Browser fetch</h2>
+
       <ul className=" list-disc pl-4 my-4">
         {pets.map((pet) => {
           return <li key={pet}>{pet}</li>
         })}
       </ul>
+
       {isLoading && (
         <p className="text-center text-sm  my-2 font-semibold text-green-700">
           loading...
         </p>
       )}
+
       {error && (
         <p className="text-center text-sm my-2 font-semibold text-red-700">
           {error}
         </p>
       )}
-    </>
+    </div>
   )
 }
 
