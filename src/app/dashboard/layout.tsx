f110787bd8ac61b6a60519423f28543a0e32da59
Serge <124575375+SergeAtZeDigitalBench@users.noreply.github.com>
2024-04-15 09:07:01 +0100

    refactor: removed login route

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,25 +4,9 @@ type Props = {
   children: ReactNode
   users: ReactNode
   articles: ReactNode
-  login: ReactNode
 }
 
-const DashboardLayout = ({
-  children,
-  users,
-  articles,
-  login,
-}: Props): JSX.Element => {
-  const isAuthenticated = false
-
-  if (!isAuthenticated) {
-    return (
-      <div className="max-w-3xl mx-auto my-8 bg-slate-200 rounded-lg p-2 min-h-[75vh] flex flex-col justify-around">
-        {login}
-      </div>
-    )
-  }
-
+const DashboardLayout = ({ children, users, articles }: Props): JSX.Element => {
   return (
     <div className="max-w-3xl mx-auto my-8 bg-slate-200 rounded-lg p-2 min-h-[75vh] flex flex-col justify-around">
       {children}
