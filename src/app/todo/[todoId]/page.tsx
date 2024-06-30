a758535378a1e9d6c83cf1ec736db0f351d851c8
Serge Basangovs <sergejs.basangovs@gmail.com>
2024-06-30 14:35:45 +0300

    feat: todo page

--- src/app/todo/[todoId]/page.tsx
+++ src/app/todo/[todoId]/page.tsx
@@ -1,14 +1,28 @@
 import { redirect } from 'next/navigation'
-import Link from 'next/link'
+import {
+  QueryClient,
+  HydrationBoundary,
+  dehydrate,
+} from '@tanstack/react-query'
 
 import type { PageProps } from '@/types'
 
 import DisplayTodo from '@/components/DisplayTodo'
+import { fetchTodoBy } from '@/lib/db/todos'
 
 const TodoPage = async ({ params }: PageProps<{ todoId: string }>) => {
   const { todoId } = params
+  const queryClient = new QueryClient()
+  await queryClient.prefetchQuery({
+    queryKey: ['todos', todoId],
+    queryFn: () => fetchTodoBy({ id: todoId }),
+  })
 
-  return <h1>Todo ID: {todoId}</h1>
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <DisplayTodo todoId={todoId} />
+    </HydrationBoundary>
+  )
 }
 
 export default TodoPage
--- src/components/DisplayTodo/index.tsx
+++ src/components/DisplayTodo/index.tsx
@@ -1,23 +1,37 @@
 'use client'
 
-import React from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 
-import { Todo } from '@/types'
+import type { Todo } from '@/types'
+
+import { getErrorMessage } from '@/lib'
+import clsx from 'clsx'
+
+const fetchTodo = async ({ id }: { id: string }) => {
+  const res = await fetch(`/api/todos/${id}`)
+
+  if (!res.ok) {
+    const errorResponse = await res.json()
+    const errorMessage = getErrorMessage(errorResponse, 'Failed to fetch todo')
+
+    throw new Error(errorMessage)
+  }
+
+  return (await res.json()) as Todo
+}
 
 interface Props {
   todoId: string
-  fetchedPromise: Promise<Todo>
 }
 
-const DisplayTodo = ({ todoId, fetchedPromise }: Props): JSX.Element | null => {
+const DisplayTodo = ({ todoId }: Props): JSX.Element | null => {
   const router = useRouter()
 
   const { data, isLoading, error } = useQuery({
     queryKey: ['todos', todoId],
-    queryFn: () => fetchedPromise,
+    queryFn: () => fetchTodo({ id: todoId }),
   })
 
   if (isLoading) {
@@ -40,10 +54,15 @@ const DisplayTodo = ({ todoId, fetchedPromise }: Props): JSX.Element | null => {
   }
 
   return (
-    <section className="max-w-md mx-auto bg-orange-200 rounded-lg p-2 flex flex-col gap-4 justify-center items-center">
+    <section
+      className={clsx(
+        'max-w-md mx-auto  rounded-lg p-2 flex flex-col gap-4 justify-center items-center',
+        data.completed ? 'bg-green-300' : 'bg-orange-200'
+      )}
+    >
       <Link
         href={`/todo/${todoId}/update`}
-        className="px-2 py-1 bg-green-500 hover:bg-green-600 active:bg-green-700 rounded"
+        className="px-2 py-1 bg-red-500 hover:bg-red-600 active:bg-red-700 rounded"
       >
         update
       </Link>
