1322c01733c9dfd95301d0da102d262a0304e209
Serge Basangovs <sergejs.basangovs@gmail.com>
2024-07-14 14:45:17 +0300

    feat: display posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,74 @@
-import { main } from '@/lib/db/seed'
+import { db } from '@/lib/db'
+
+type PostSummary = {
+  id: string
+  createdAt: string | null
+  title: string
+  content: string
+  status: 'draft' | 'published' | 'hidden'
+  user: {
+    username: string
+  }
+}
+
+const getPosts = async (): Promise<[PostSummary[], null] | [null, string]> => {
+  try {
+    const posts = await db.query.posts.findMany({
+      with: {
+        user: {
+          columns: {
+            username: true,
+            id: false,
+            email: false,
+            isEmailConfirmed: false,
+            password: false,
+            createdAt: false,
+          },
+        },
+      },
+      columns: {
+        userId: false,
+      },
+    })
+
+    return [posts, null]
+  } catch (error) {
+    console.warn(error)
+    return [null, 'Failed to query the posts']
+  }
+}
 
 const Homepage = async () => {
+  const [posts, dbError] = await getPosts()
+
   return (
     <>
       <h1 className="text-3xl font-bold underline text-center">
         NEXT V.14 POLYGON
       </h1>
 
-      <form>
-        <button
-          type="submit"
-          formAction={async () => {
-            'use server'
-            await main()
-          }}
-          className="px-2 py-1 rounded bg-green-500 hover:bg-green-600"
-        >
-          seed database
-        </button>
-      </form>
+      {dbError && (
+        <p className="my-4 text-red-700 font-semibold text-center">{dbError}</p>
+      )}
+
+      <ul className="grid gap-4 grid-cols-gallery grid-rows-[repeat(4,_auto)] px-2">
+        {posts &&
+          posts.map(({ id, title, content, createdAt, user }) => {
+            return (
+              <li
+                key={id}
+                className="grid grid-rows-[subgrid] gap-2 rounded-xl p-2 bg-slate-200 row-span-4"
+              >
+                <h3 className="text-xl font-semibold text-center">{title}</h3>
+                <small className="text-xs">Created: {createdAt}</small>
+                <small className="text-xs font-semibold">
+                  By: {user.username}
+                </small>
+                <p>{content}</p>
+              </li>
+            )
+          })}
+      </ul>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        gallery: 'repeat(auto-fit, minmax(250px, 1fr))',
+        pages: 'repeat(auto-fit, minmax(350px, 1fr))',
+        'gutter-grid': '1fr min(64rem,100%) 1fr',
+      },
+      gridColumn: {
+        'full-bleed': '1 / 4',
+      },
+    },
   },
   plugins: [],
 };
